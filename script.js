92083cc6186a129d927f6298046e0af47b264f42
EduardoAugustoFReis <122159202+EduardoAugustoFReis@users.noreply.github.com>
2023-05-19 21:17:18 -0300

    Exercício 2 média dos alunos
     criar uma lista de estudantes e, cada estudante dentro dessa lista, deverá conter os seguintes dados:
    - nome;
    - nota da primeira prova;
    - nota da segunda prova.
    - [ ]  Crie uma **função** que irá calcular a média das notas de cada aluno;
    - [ ]  Supondo que a média, para esse concurso é **7**, verifique **se** cada aluno obteve sucesso ou não em entrar no concurso e mostre uma mensagem na tela.

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-let numberOne = prompt("Digite um número: ");
-let numberTwo = prompt("Digite um segundo número: ")
+let students = [
+{
+    name: "Ana",
+    score1: 8.2,
+    score2:  7.6
+},
+{
+  name: "Daniel",
+  score1: 7.4,
+  score2:  6.7
+},
+{
+  name: "Caio",
+  score1: 3.7,
+  score2:  5.8
+}
+]
 
-numberOne = Number(numberOne);
-numberTwo = Number(numberTwo);
-
-let add = numberOne + numberTwo;
-let sub = numberOne - numberTwo;
-let mult = numberOne * numberTwo;
-let div = numberOne / numberTwo;
-let rest = numberOne % numberTwo;
-
-alert(`A soma entre os números digitados foi de: ${add}`);
-alert(`A subtração entre os números digitados foi de: ${sub}`);
-alert(`A multiplicação entre os números digitados foi de: ${mult}`);
-alert(`A divisão entre os números digitados foi de: ${div}`);
-alert(`O resto da divisão entre os números digitados foi de: ${rest}`);
-
-if(add %2 == 0){
-  alert("Número par.");
-}else{
-  alert("Número ímpar.");
+function avarage(stud){
+  let avg = ((stud.score1 + stud.score2) / 2).toFixed(2);
+  
+  if(avg >=7){
+    alert(`
+    A média do(a) aluno(a) ${stud.name} é: ${avg} 
+    Parabéns, ${stud.name} você foi aprovado(a) no concurso
+    `)
+  }else{
+    alert(`
+    A média do(a) aluno(a) ${stud.name} é: ${avg}
+    Não foi dessa vez, ${stud.name} ! Tente novamnete
+    `)
+  }
 }
 
-if(numberOne == numberTwo){
-  alert("Os números digitados são iguais");
-}else{
-  alert("Os números digitados são diferentes");
+for(let stud of students){
+  avarage(stud);
 }
