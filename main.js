f0463aa912c7761491251f052d31e73870e1a175
EduardoAugustoFReis <122159202+EduardoAugustoFReis@users.noreply.github.com>
2023-07-10 18:47:54 -0300

    Desafio focus time

--- main.js
+++ main.js
@@ -1,34 +1,82 @@
-import {modal} from "./modals.js"
-import {alertError} from "./alert-error.js"
-import {notNUmber, calculateIMC} from "./utils.js";
+import sounds from "./sounds.js";
 
-const form = document.querySelector('form');
-const inputWeight = document.querySelector('#weight');// pegar o input
-const inputHeight = document.querySelector('#height'); 
+const buttonPlay = document.querySelector('.play');
+const buttonStop = document.querySelector('.stop');
+const minutesDisplay = document.querySelector('.minutes');
+const secondsDisplay = document.querySelector('.seconds');
+const buttonPlus = document.querySelector(".plus");
+const buttonMinus = document.querySelector(".minus");
 
-form.onsubmit = (event)=>{
+let timeOut;
 
-  event.preventDefault();
+const buttonTree = document.querySelector(".tree");
+const buttonCloud = document.querySelector(".cloud");
+const buttonHouse = document.querySelector(".house");
+const buttonFire = document.querySelector(".fire");
 
-  let weight = inputWeight.value;// pegar o valor do input
-  let height = inputHeight.value;
+const bgSound = sounds()
 
-  let weightOrHeightIsNotANumber = notNUmber(weight) || notNUmber(height); // verificar se é um tipo number
+function countdown(){
+  timeOut = setTimeout(function() {
+    let seconds =  Number(secondsDisplay.textContent);
+    let minutes = Number(minutesDisplay.textContent);
 
-  if(weightOrHeightIsNotANumber){ // se for "true" dispara o alerta de erro
-    alertError.open();
-    return;
+    if(minutes <=0){ // ponto de parada
+      secondsDisplay.textContent = "00";
+      return;
+    }
+
+    if( seconds <= 0 ) {
+      seconds = 60;
+      minutesDisplay.textContent = String(minutes - 1).padStart(2,"0"); // atualiza os minutos para menos 1
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2,"0"); // atualiza os segundos para menos 1
+    
+    countdown()
+  }, 1000)
+}
+
+function updateTimePlus(){
+  minutesDisplay.textContent = Number(minutesDisplay.textContent) + 5;
+}
+
+function updateTimeMinus(){
+  minutesDisplay.textContent = Number(minutesDisplay.textContent) - 5;
+  
+  if(minutesDisplay.textContent <= 0){
+    minutesDisplay.textContent = "00"
   }
-
-  alertError.close() // retirar o alerta de erro
-
-  let result = calculateIMC(weight,height);
-  let msg = `Seu IMC é de ${result}`
-
-  modal.message.innerHTML = msg; 
-  modal.open() // abrir o modal
 }
 
-inputWeight.oninput = () => { // fechar o alerta de erro ao interagir com o input weight
-  alertError.close()
-}
+buttonPlay.addEventListener("click", function(){
+  countdown();
+});
+
+buttonStop.addEventListener("click", function(){
+  clearTimeout(timeOut); // parada da função setTimeout
+})
+
+buttonPlus.addEventListener("click", function(){
+  updateTimePlus();
+})
+
+buttonMinus.addEventListener("click", function(){
+  updateTimeMinus();
+})
+
+buttonTree.addEventListener("click", function(){
+  bgSound.pressButtonTree()
+})
+
+buttonCloud.addEventListener("click", function(){
+  bgSound.pressButtonCloud();
+})
+
+buttonHouse.addEventListener("click", function(){
+  bgSound.pressButtonHouse();
+})
+
+buttonFire.addEventListener("click", function(){
+  bgSound.pressButtonFire();
+})
